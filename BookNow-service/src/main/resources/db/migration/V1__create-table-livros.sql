9a606188d73c2f2838a56c3a202b2e90821f85df
Micael Fone <micaelfone0@gmail.com>
2024-08-31 19:33:24 -0300

    Mudando a estrutura do banco de dados da tabela livros

--- BookNow-service/src/main/resources/db/migration/V1__create-table-livros.sql
+++ BookNow-service/src/main/resources/db/migration/V1__create-table-livros.sql
@@ -1,9 +1,28 @@
-CREATE TABLE Livros(
-     id BIGINT NOT NULL AUTO_INCREMENT NOT NULL,
-     nome VARCHAR(50)  UNIQUE NOT NULL,
-     descricao VARCHAR(250) NOT NULL,
-     autor VARCHAR(50) NOT NULL,
-     datapublicacao DATE NOT NULL,
-     classificacao_livro VARCHAR(50) NOT NULL,
-     primary key (id)
-)
+ DROP TABLE IF EXISTS `livros`;
+ /*!40101 SET @saved_cs_client     = @@character_set_client */;
+ /*!50503 SET character_set_client = utf8mb4 */;
+ CREATE TABLE `livros` (
+   `idLivros` int NOT NULL AUTO_INCREMENT,
+   `NomeLivro` varchar(100) NOT NULL,
+   `descricao` varchar(250) DEFAULT NULL,
+   `Autor` varchar(50) NOT NULL,
+   `dataPublicacao` date NOT NULL,
+   `classificacaoLivro` varchar(50) NOT NULL,
+   `imagem` varchar(250) DEFAULT NULL,
+   `ebook` varchar(1000) NOT NULL,
+   `pontos` int NOT NULL,
+   PRIMARY KEY (`idLivros`),
+   UNIQUE KEY `idLivros_UNIQUE` (`idLivros`),
+   UNIQUE KEY `NomeLivro_UNIQUE` (`NomeLivro`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+ /*!40101 SET character_set_client = @saved_cs_client */;
+
+ --
+ -- Dumping data for table `livros`
+ --
+
+ LOCK TABLES `livros` WRITE;
+ /*!40000 ALTER TABLE `livros` DISABLE KEYS */;
+ /*!40000 ALTER TABLE `livros` ENABLE KEYS */;
+ UNLOCK TABLES;
+ /*!40103 SET TIME_

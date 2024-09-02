e8b3470f6bb7c7006b39457476c0b856c64d30c4
Micael Fone <micaelfone0@gmail.com>
2024-09-01 21:53:35 -0300

    Arrumando DTO de cadastro

--- BookNow-service/src/main/java/APi_BookNowService_livro/BookNow/DTO/DadosCadastrarLivros.java
+++ BookNow-service/src/main/java/APi_BookNowService_livro/BookNow/DTO/DadosCadastrarLivros.java
@@ -2,30 +2,29 @@ package APi_BookNowService_livro.BookNow.DTO;
 import APi_BookNowService_livro.BookNow.model.Arquivos.ArquivosLivro;
 import APi_BookNowService_livro.BookNow.model.Livro.LivroClasse;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
 
 public record DadosCadastrarLivros(
-        Long id,
+        Long idLivro,
         @NotBlank
-        @Column(unique = true)
-        String nome,
+        String NomeLivro,
         @NotBlank
         String descricao,
         @NotBlank
-        String autor,
+        String Autor,
         @NotNull
-        Date datapublicacao,
+        Date dataPublicacao,
         @NotBlank
-        String classificacao_livro,
+        String classificacaoLivro,
         ArquivosLivro arquivos,
         @NotNull
         Integer pontos
 ) {
         public DadosCadastrarLivros(LivroClasse livro) {
-               this(livro.getId(),livro.getNome(),livro.getDescricao(),livro.getDescricao(),livro.getDatapublicacao(),livro.getAutor(),livro.getArquivos(),livro.getPontos());
+               this(livro.getIdLivro(),livro.getNomeLivro(),livro.getDescricao(),livro.getAutor(),livro.getDataPublicacao(),livro.getClassificacaoLivro(),livro.getArquivos(),livro.getPontos());
         }
-
 }
